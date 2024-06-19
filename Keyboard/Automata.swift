1cacc4171a89f330a6391f9cc3a8ea99da44a5a2
n30gu1 <n@n30gu1.com>
2024-06-19 20:09:44 +0900

    chore: add Swift 6 support

--- Keyboard/Automata.swift
+++ Keyboard/Automata.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 // MARK: Variables and Initializer
+@MainActor
 final class Automata: ObservableObject {
     static let chosungTable = ["ㄱ": 0, "ㄲ": 1, "ㄴ": 2, "ㄷ": 3, "ㄸ": 4, "ㄹ": 5, "ㅁ": 6, "ㅂ": 7, "ㅃ": 8, "ㅅ": 9, "ㅆ": 10, "ㅇ": 11, "ㅈ": 12, "ㅉ": 13, "ㅊ": 14, "ㅋ": 15, "ㅌ": 16, "ㅍ": 17, "ㅎ": 18]
 //    static let jungsungTable = ["ㅏ": 0, "ㅐ": 1, "ㅑ": 2, "ㅒ": 3, "ㅓ": 4, "ㅔ": 5, "ㅕ": 6, "ㅖ": 7, "ㅗ": 8, "ㅘ": 9, "ㅙ": 10, "ㅚ": 11, "ㅛ": 12, "ㅜ": 13, "ㅝ": 14, "ㅞ": 15, "ㅟ": 16, "ㅠ": 17, "ㅡ": 18, "ㅢ": 19, "ㅣ": 20]
@@ -84,7 +85,7 @@ extension Automata {
             case ["ㄹ", "ㅅ"]:
                 12
             case ["ㄹ", "ㅌ"]:
-                13
+                    13
             case ["ㄹ", "ㅍ"]:
                 14
             case ["ㄹ", "ㅎ"]:
